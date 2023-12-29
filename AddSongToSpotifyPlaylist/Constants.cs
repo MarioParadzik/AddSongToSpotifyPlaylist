5e6e8a56e9a7150a65148feaf867848cbcbd5bb7
MarioParadzik <80365569+MarioParadzik@users.noreply.github.com>
2023-12-29 03:39:37 +0100

    Update Constants.cs

--- AddSongToSpotifyPlaylist/Constants.cs
+++ AddSongToSpotifyPlaylist/Constants.cs
@@ -9,23 +9,23 @@ internal static class Endpoints
 
 internal static class EnviornmentVariables
 {
-    internal const string ClientId = nameof(ClientId);
-    internal const string ClientSecret = nameof(ClientSecret);
-    internal const string VaultUri = nameof(VaultUri);
-    internal const string Passphrase = nameof(Passphrase);
-    internal const string PlaylistId = nameof(PlaylistId);
-    internal const string RedirectUri = nameof(RedirectUri);
+    internal const string ClientId = "ClientId";
+    internal const string ClientSecret = "ClientSecret";
+    internal const string VaultUri = "VaultUri";
+    internal const string Passphrase = "Passphrase";
+    internal const string PlaylistId = "PlaylistId";
+    internal const string RedirectUri = "RedirectUri";
 }
 
 internal static class FunctionNames
 {
-    internal const string SpotifyAddSongToPlaylistFunction = nameof(SpotifyAddSongToPlaylistFunction);
-    internal const string SpotifyAuthorizationFunction = nameof(SpotifyAuthorizationFunction);
-    internal const string SpotifyCallbackFunction = nameof(SpotifyCallbackFunction);
+    internal const string SpotifyAddSongToPlaylistFunction = "SpotifyAddSongToPlaylistFunction";
+    internal const string SpotifyAuthorizationFunction = "SpotifyAuthorizationFunction";
+    internal const string SpotifyCallbackFunction = "SpotifyCallbackFunction";
 }
 internal static class KeyVaultName
 {
-    internal const string AccessToken = nameof(AccessToken);
-    internal const string RefreshToken = nameof(RefreshToken);
-    internal const string ExpiresIn = nameof(ExpiresIn);
+    internal const string AccessToken = "AccessToken";
+    internal const string RefreshToken = "RefreshToken";
+    internal const string ExpiresIn = "ExpiresIn";
 }
